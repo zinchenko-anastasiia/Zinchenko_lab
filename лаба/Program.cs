d76152cf3e02937e5e00d9fd60e1e4a4e0063816
zinchenko-anastasiia <zincenkonasta6@gmail.com>
2021-09-15 17:36:03 +0300

    Добавьте файлы проекта.

---
+++ лаба/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace лабораторна1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            double R1, R2;
+            double R;
+            Console.WriteLine("Введiть опiр першого резистора: ");
+            R1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введiть опiр другого резистора: ");
+            R2 = int.Parse(Console.ReadLine());
+            R = (1 / R1) + (1 / R2);
+            Console.WriteLine($"Отже, загальний опiр ланцюга {R}");
+
+        }         
+    }
+}
