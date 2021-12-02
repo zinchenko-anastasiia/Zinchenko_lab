fb5003494fc6afe633b9d35a0cdbc6ee067df4b9
zinchenko-anastasiia <zincenkonasta6@gmail.com>
2021-12-02 10:00:23 +0200

    лаби

---
+++ lab_4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[3, 3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("arr[" + i + "," + j + "]: ");
+                    arr[i, j] = int.Parse(Console.ReadLine());
+
+                }
+
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int currentValue = arr[i, j];
+                    if (currentValue % 2 == 0)
+                    {
+                        Console.WriteLine(arr[i, j] + " even");
+                    }
+
+                }
+            }
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        int currentValue = arr[i, j];
+                        if (currentValue % 2 != 0)
+                        {
+                            Console.WriteLine(arr[i, j] + " odds");
+                        }
+                    }
+
+                }
+            }
+    }
+}
+    
+
---
+++ laba_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace laba_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            bool check = true;
+            Console.WriteLine("цiле число");
+            n = int.Parse(Console.ReadLine());
+            if (9 < n && n < 100)
+            {
+                if (n % 2 == 0)
+                {
+                    Console.Write(check);
+                }
+                else
+                {
+                    Console.Write(false);
+                }
+            }
+
+            else
+            {
+                Console.Write(false);
+            }
+
+
+
+        }
+    }
+}
